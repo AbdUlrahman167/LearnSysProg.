3811ee5cd6225a04f772d5eec3525bd051780830
AbdUlrahman <78540707+AbdUlrahman167@users.noreply.github.com>
2022-09-12 11:42:06 +0200

    Update femtoshell.c

--- femtoshell.c
+++ femtoshell.c
@@ -2,17 +2,17 @@
 #include <string.h>
 int main(int argc, char const *argv[])
 {
-	char str[50];
-	char strR[] = "exit";
-	do {
+        char str[50];
+        char strR[] = "exit";
+        do {
  printf("Ana Gahez ya Basha > ");
-	scanf("%s",str);
-	if (strcmp(str,strR) != 0)
-			printf("You said : %s\n",str);
-		else
-			{printf("Good Bye :)\n");
+        fgets(str,50,stdin);
+        if (strcmp(str,strR) != 0)
+                        printf("You said : %s\n",str);
+                else
+                        {printf("Good Bye :)\n");
             break;}
 }
 while(1);
-			return 0;
+                        return 0;
 }
